1c2aa99cf2c8688dde9bc53cfc2f1b467731c088
gabrieledalcolle <gabrieledalcolle@users.noreply.github.com>
2020-01-12 15:44:53 +0100

    Update menu.lua

--- Plastiblob/menu.lua
+++ Plastiblob/menu.lua
@@ -19,7 +19,7 @@ local playBtn
 local function onPlayBtnRelease()
 	
 	-- go to level1.lua scene
-	composer.gotoScene( "level1", "fade", 1000 )
+	composer.gotoScene( "level1", "fade", 500 )
 	
 	return true	-- indicates successful touch
 end
