3026109a32912176e4bc060c57155b4b2ca26298
alebortola <alebortola@gmail.com>
2020-02-04 22:47:20 +0100

    Iniziato lavoro per cancellare dalla scena le cosse

--- Plastiblob/main.lua
+++ Plastiblob/main.lua
@@ -13,5 +13,5 @@ local composer = require "composer"
 -- load menu screen
 --NON CANCELLARE LA PROSSIMA RIGA
 --composer.gotoScene( "menu" )
---composer.gotoScene( "levels.level1" )
-composer.gotoScene( "levels.level2" )
+composer.gotoScene( "levels.level1" )
+--composer.gotoScene( "levels.level2" )
