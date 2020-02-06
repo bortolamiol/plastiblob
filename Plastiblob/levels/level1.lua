85f52145c07e3678883efad838c77ba701ad2d4e
alebortola <alebortola@gmail.com>
2020-02-06 18:04:29 +0100

    il rettangolo del ground ora non ha colore

--- Plastiblob/levels/level1.lua
+++ Plastiblob/levels/level1.lua
@@ -16,9 +16,10 @@ local table_plasticbag = {}
 local button_home --bottone per uscire dal livello e tornare alla button_home dei livelli
 local stop =  0 --variabile che servirà per capire se stoppare il gioco
 local callingEnemies
+local castle
 local callingPlasticbag
 local timeplayed  --varaiabile che misura da quanti secondi sono all'interno del gioco e farà cambiare la velocità
-local timeToPlay = 100 --variabile che conterrà quanto l'utente dovrà sopravvivere all'interno del gioco
+local timeToPlay = 10 --variabile che conterrà quanto l'utente dovrà sopravvivere all'interno del gioco
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
@@ -45,7 +46,7 @@ function scene:show( event )
 		physics.start()
 		-- Overlays collision outlines on normal display objects
         physics.setGravity( 0,20 )
-        physics.setDrawMode( "hybrid" )
+        --physics.setDrawMode( "hybrid" )
 		-- The default Corona renderer, with no collision outlines
 		--physics.setDrawMode( "normal" )
 		-- Shows collision engine outlines only
@@ -63,7 +64,9 @@ function scene:show( event )
         elseif(tutorial == 1) then
             --ELIMINARE IL GRUPPO DEL TUTORIAL 
             -- INIZIALIZZO LE VARIABILI CHE VERRANNO USATE NEL GIOCO
-               local secondsPlayed = 0
+               local secondsPlayed = 0 --quanti secondi sono passati dall'inizio del gioco
+               local castleAppared = 0 --variabile fuffa che mi servirà per controllare se il castello è già apparso sullo schermo una volta
+
             -- VARIABILI PER LO SFONDO DI BACKGROUND {
                 local _w = display.actualContentWidth  -- Width of screen
                 local _h = display.actualContentHeight  -- Height of screen
@@ -86,7 +89,7 @@ function scene:show( event )
 
                 ------------------------------------------------------------
                 -- VARIABILI MOLTO IMPORTANTI PER IL GIOCO: VELOCITA' DI GIOCO
-                local enemySpeed_max = 14 -- massima velocità di spostamento del nemico
+                local enemySpeed_max = 8 -- massima velocità di spostamento del nemico
                 local enemySpeed_min = 2 -- minima velocità di spostamento del nemico
                 local enemySpeed = enemySpeed_min --velocità di spostamento del nemico
 
@@ -102,8 +105,9 @@ function scene:show( event )
             --VARIABILI PER GLI ELEMENTI DELLO SCHERMO{
             local groundHeight = 100
             local ground = display.newRect( 0, 0,99999, groundHeight )
+            ground:setFillColor(0,0,0,0)
             ground.name = "ground"
-            group_elements:insert(ground)
+            --group_elements:insert(ground)
             ground.x = display.contentCenterX
             ground.y = display.contentHeight- groundHeight/2
             physics.addBody(ground, "static",{bounce=0, friction=1 } )
@@ -166,7 +170,14 @@ function scene:show( event )
                 { name="plastic", sheet=plasticbagSheet, start=1, count=4, time=500, loopCount=0 }
             }
             local plasticbagTimeSpawn = 5000
+            
 
+            --CASTELLO DI SABBIA IN CUI ENTRERO' A FINE LIVELLO
+            castle = display.newImageRect( "immagini/livello-1/sandcastle-duck.png", 300, 300 )
+            castle.anchorX =  0
+            castle.anchorY =  0
+            castle.x = display.actualContentWidth - 100
+            castle.y = 80
             --}
             
 
@@ -336,6 +347,12 @@ function scene:show( event )
                 sprite:play()
             end
             ------------------------------------------------
+            local function castleScroll(self, event) --funzione per far apparire nello schermo un castello in cui il blob entrerà
+                if stop == 0 then
+                    self.x = self.x - (enemySpeed*2)
+                end
+            end
+            ----------------------------------------------
             local function increaseGameSpeed(event)
                 secondsPlayed = secondsPlayed + 1 --ogni secondo che passa aumento questa variabile che tiene conto di quanto tempo è passato
                 print(secondsPlayed)
@@ -347,7 +364,13 @@ function scene:show( event )
                     --cambio della velocità del nostro nemico [da 2 a 12] --> secondi passati : secondi totali = x : 12 (ritornerà un numero da 1 a 12)
                     local x_enemySpeed = ((enemySpeed_max * secondsPlayed)/timeToPlay)
                     enemySpeed = enemySpeed_min + x_enemySpeed --la velocità è data dalla velocità minima (2) + il risultato della proporzione
+                end
+                if(secondsPlayed >= timeToPlay) then
+                    if (castleAppared == 0 ) then
+                        castleAppared = 1
+                        print("ORA APPARE IL CASTELLO")
                     end
+                end
             end
             -- }
             --bottone per uscire dal livello e tornare al menu del livelli
