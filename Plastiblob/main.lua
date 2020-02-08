a1e20ec57556a3668ee65feaabd089e7ba74ba53
alebortola <alebortola@gmail.com>
2020-02-08 10:49:16 +0100

    Tolto splash

--- Plastiblob/main.lua
+++ Plastiblob/main.lua
@@ -12,5 +12,5 @@ local composer = require "composer"
 
 -- load menu screen
 --NON CANCELLARE LA PROSSIMA RIGA
-composer.gotoScene( "menu" )
---composer.gotoScene( "levels.level1" )
+--composer.gotoScene( "menu" )
+composer.gotoScene( "levels.level1" )
