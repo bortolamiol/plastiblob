71167e4ebf13bb2c332f251c9e10d25e2edbdd43
McBerna <lorenzobernardisux@gmail.com>
2020-02-13 16:55:20 +0100

    spostato immagini

---
+++ Plastiblob/levels/storylevel1.lua
@@ -0,0 +1,6 @@
+-- mostro la prima immagine
+local background = display.newImageRect( "immagini/livello-1/storia1/1.png", display.actualContentWidth, display.actualContentHeight )
+background.anchorX = 0
+background.anchorY = 0
+background.x = 0 + display.screenOriginX 
+background.y = 0 + display.screenOriginY
